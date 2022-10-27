4b3a484435989cc520a35681e6dead6e2f836538
pedroandradee <pedro.h8237@gmail.com>
2022-10-27 11:14:15 -0300

    First commit

---
+++ src/main/java/com/pedrohenrique/workshopspirngmongodb/WorkshopspringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.pedrohenrique.workshopspirngmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringmongodbApplication.class, args);
+	}
+
+}
