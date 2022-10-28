2b8a536a62cbea78936ee063d9d98b02a4808e33
pedroandradee <pedro.h8237@gmail.com>
2022-10-28 11:11:39 -0300

    Simple query with @Query

--- src/main/java/com/pedrohenrique/workshopspirngmongodb/repository/PostRepository.java
+++ src/main/java/com/pedrohenrique/workshopspirngmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.pedrohenrique.workshopspirngmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedrohenrique.workshopspirngmongodb.domain.Post;
@@ -10,6 +11,9 @@ import com.pedrohenrique.workshopspirngmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
     
+    @Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+    List<Post> findByTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
