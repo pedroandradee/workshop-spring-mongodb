1a09d4ba892635802dfddd0080be43240e86792d
pedroandradee <pedro.h8237@gmail.com>
2022-10-28 09:15:13 -0300

    Get user by id

--- src/main/java/com/pedrohenrique/workshopspirngmongodb/services/UserServices.java
+++ src/main/java/com/pedrohenrique/workshopspirngmongodb/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.pedrohenrique.workshopspirngmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pedrohenrique.workshopspirngmongodb.domain.User;
 import com.pedrohenrique.workshopspirngmongodb.repository.UserRepository;
+import com.pedrohenrique.workshopspirngmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -16,6 +18,14 @@ public class UserServices {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User findById(String id) {
+        User user = userRepository.findOne(id);
+        if (user == null) {
+            throw new ObjectNotFoundException("Usuário não encontrado! id: " + id);
+        }
+        return user;
     }
 
 }
