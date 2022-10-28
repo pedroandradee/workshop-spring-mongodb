1949c09b2c35c56912a7b12aefbe3b8c0dddbba8
pedroandradee <pedro.h8237@gmail.com>
2022-10-28 10:14:58 -0300

    Creation of Post entity with User nested

--- src/main/java/com/pedrohenrique/workshopspirngmongodb/config/Instantiation.java
+++ src/main/java/com/pedrohenrique/workshopspirngmongodb/config/Instantiation.java
@@ -1,10 +1,16 @@
 package com.pedrohenrique.workshopspirngmongodb.config;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+
+import com.pedrohenrique.workshopspirngmongodb.domain.Post;
+import com.pedrohenrique.workshopspirngmongodb.repository.PostRepository;
 
 import com.pedrohenrique.workshopspirngmongodb.domain.User;
 import com.pedrohenrique.workshopspirngmongodb.repository.UserRepository;
@@ -15,17 +21,25 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
         
-        // userRepository.deleteAll();
+        userRepository.deleteAll();
 
-        // User u1 = new User(null, "Marie Brown", "marie@email.com");
-        // User u2 = new User(null, "John Doe", "john@email.com");
-        // User u3 = new User(null, "Bob Grey", "bob@email.com");
+        User u1 = new User(null, "Marie Brown", "marie@email.com");
+        User u2 = new User(null, "John Doe", "john@email.com");
+        User u3 = new User(null, "Bob Grey", "bob@email.com");
 
-        // userRepository.saveAll(Arrays.asList(u1, u2, u3));
-
+        userRepository.saveAll(Arrays.asList(u1, u2, u3));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+        Post p1 = new Post(null, sdf.parse("01/10/2018"), "Bom dia!", "Dia incrível aqui em cidade 1!!!", u1);
+        Post p2 = new Post(null, sdf.parse("28/10/2018"), "Partiu viagem!", "Vou viajar para São Paulo!!!", u1);
+        
+        postRepository.saveAll(Arrays.asList(p1, p2));
     }
     
 }
